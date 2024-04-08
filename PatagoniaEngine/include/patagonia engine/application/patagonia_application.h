2a65d86176fa5ab4bd63f10927c1482f1d58477b
Facundo <facuwilli@gmail.com>
2024-04-08 16:45:08 -0300

    Add a engine application class

---
+++ PatagoniaEngine/include/patagonia engine/application/patagonia_application.h
@@ -0,0 +1,17 @@
+#ifndef PATAGONIA_APPLICATION_H
+#define PATAGONIA_APPLICATION_H
+
+namespace PatagoniaEngine
+{
+	class PatagoniaApplication
+	{
+	public:
+		PatagoniaApplication();
+		~PatagoniaApplication();
+
+	private:
+
+	};	
+}
+
+#endif
---
+++ PatagoniaEngine/source/application/patagonia_application.cpp
@@ -0,0 +1,14 @@
+#include "../../include/patagonia engine/application/patagonia_application.h"
+
+namespace PatagoniaEngine
+{
+	PatagoniaApplication::PatagoniaApplication()
+	{
+
+	}
+
+	PatagoniaApplication::~PatagoniaApplication()
+	{
+
+	}
+}
