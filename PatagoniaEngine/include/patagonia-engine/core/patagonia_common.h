dae40e4fc0640eb8ec79aaca63b268ee4b9fde21
Facundo <facuwilli@gmail.com>
2024-04-09 10:11:33 -0300

    Add macro to finish program execution when a conditins is met

--- PatagoniaEngine/include/patagonia-engine/core/patagonia_common.h
+++ PatagoniaEngine/include/patagonia-engine/core/patagonia_common.h
@@ -1,6 +1,8 @@
 #ifndef PATAGONIA_COMMON_H
 #define PATAGONIA_COMMON_H
 
+#include <stdlib.h>
+
 #define PATAGONIA_WINDOWS (_WIN32 || _WIN64)
 
 #ifdef PATAGONIA_WINDOWS
@@ -10,6 +12,11 @@
 #else
 	#define PATAGONIA_API __declspec(dllimport)
 #endif
+
+#define PATAGONIA_ASSERT(x)\
+{\
+	if ((x) == 0) {__debugbreak(); abort();}\
+}
 
 #endif
 
