5e549621db40311b92e6f41f4270d26b130c04df
Facundo <facuwilli@gmail.com>
2024-04-09 00:34:44 -0300

    Add prototype of my engine application class

--- PatagoniaEngine/include/patagonia-engine/application/patagonia_application.h
+++ PatagoniaEngine/include/patagonia-engine/application/patagonia_application.h
@@ -7,10 +7,20 @@ namespace PatagoniaEngine
 	{
 	public:
 		PatagoniaApplication();
-		~PatagoniaApplication();
+		virtual ~PatagoniaApplication();
+
+		static PatagoniaApplication* getEngineApplication();
+		
+		virtual void onEngineInit() = 0;
+		virtual void onEngineQuit() = 0;
+
+		void startEngineApplication();
+		void finishEngineApplication();
 
 	private:
+		const char* DoubleIntanceCreationRuntimeErrorMessage = "The engine application was already created, there can not be two intances at the same time!";
 
+		static PatagoniaApplication* m_engineApplicationInstance;
 	};	
 }
 
--- PatagoniaEngine/source/application/patagonia_application.cpp
+++ PatagoniaEngine/source/application/patagonia_application.cpp
@@ -1,13 +1,39 @@
 #include "patagonia-engine/application/patagonia_application.h"
 
+#include <stdexcept>
+
 namespace PatagoniaEngine
 {
+	PatagoniaApplication* PatagoniaApplication::m_engineApplicationInstance = nullptr;
+
 	PatagoniaApplication::PatagoniaApplication()
+	{
+		if(!m_engineApplicationInstance)
+		{
+			m_engineApplicationInstance = this;
+		}
+		else
+		{
+			throw std::runtime_error(DoubleIntanceCreationRuntimeErrorMessage);
+		}
+	}
+
+	PatagoniaApplication::~PatagoniaApplication()
 	{
 
 	}
 
-	PatagoniaApplication::~PatagoniaApplication()
+	PatagoniaApplication* PatagoniaApplication::getEngineApplication()
+	{
+		return m_engineApplicationInstance;
+	}
+	
+	void PatagoniaApplication::startEngineApplication()
+	{
+
+	}
+	
+	void PatagoniaApplication::finishEngineApplication()
 	{
 
 	}
